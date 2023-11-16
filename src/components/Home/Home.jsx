6f8996b2ea60c0635a2d0dbdad7de13ee291c0a6
Abhinav Singh <abhinavas430@gmail.com>
2023-11-16 20:21:55 +0530

    Image side change

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,14 @@ export default function Home() {
         <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
             <section>
                 <div className="px-2 lg:flex lg:flex-row lg:items-center">
-                    <div className="w-full lg:w-1/2 order-1 sm:order-2">
+                    <div className="w-full lg:w-1/2 order-2 sm:order-1">
+                        <img
+                            src="https://github.com/harikishansingh/harikishansingh.github.io/blob/main/src/assets/7.jpeg?raw=true"
+                            alt="ManWith Laptop"
+                            className="h-full w-full rounded-md object-cover w-1/2"
+                        />
+                    </div>
+                    <div className="w-full lg:w-1/2 sm:order-2">
                         <div className="my-10 lg:my-0 lg:px-10">
                             <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
                                 Hey there, I'm Hari
@@ -22,20 +29,12 @@ export default function Home() {
                                 >
                                     Download CV
                                 </a>
-
                             </div>
                         </div>
-                    </div>
-                    <div className="w-full lg:w-1/2 sm:order-2">
-                        <img
-                            src="https://github.com/harikishansingh/harikishansingh.github.io/blob/main/src/assets/7.jpeg?raw=true"
-                            alt="ManWith Laptop"
-                            className="h-full w-full rounded-md object-cover w-1/2"
-                        />
                     </div>
                 </div>
             </section>
         </div>
+    );
 
-    )
 }
